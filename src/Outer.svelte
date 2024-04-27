09d18effaaead447abb53e6d745927c782242344
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-27 19:35:00 +0700

    2/8. Events - Event forwarding

---
+++ src/Outer.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+	import Inner from './Inner.svelte';
+</script>
+
+<!-- in here you need add on:message for Outer component can access the event createEventDispatcher, because event dipatcher/createEventDIspatcher don't bubble -->
+<Inner on:message/>
