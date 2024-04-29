56eff4b2c7678a50f0c593af02c4c99bbc2bc4c2
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-29 07:30:24 +0700

    4/1. Stores & Derived State - Readable stores.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,15 @@
 <script>
-	import { onDestroy } from 'svelte';
-	import { count } from './stores.js';
-	import Incrementer from './Incrementer.svelte';
-	import Decrementer from './Decrementer.svelte';
-	import Resetter from './Resetter.svelte';
+	import { time } from './stores.js';
 
-	// let count_value;
-
-	// const unsubscribe = count.subscribe((value) => {
-	// 	count_value = value;
-	// });
-
-	// //this is for avoid memory leaks
-	// onDestroy(unsubscribe);
+	const formatter = new Intl.DateTimeFormat(
+		'en',
+		{
+			hour12: true,
+			hour: 'numeric',
+			minute: '2-digit',
+			second: '2-digit'
+		}
+	);
 </script>
 
-<!-- $count is for shortcut subscribe/listen store data, and without memory leaks -->
-<h1>The count is {$count}</h1>
-
-<Incrementer />
-<Decrementer />
-<Resetter />
+<h1>The time is {formatter.format($time)}</h1>
