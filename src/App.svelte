8389145218cf9ae8590c0bc95c462f32d00e12fd
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-29 07:25:28 +0700

    4/1. Stores & Derived State - auto subscriptions.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,22 @@
 <script>
+	import { onDestroy } from 'svelte';
 	import { count } from './stores.js';
 	import Incrementer from './Incrementer.svelte';
 	import Decrementer from './Decrementer.svelte';
 	import Resetter from './Resetter.svelte';
 
-	let count_value;
+	// let count_value;
 
-	count.subscribe((value) => {
-		count_value = value;
-	});
+	// const unsubscribe = count.subscribe((value) => {
+	// 	count_value = value;
+	// });
+
+	// //this is for avoid memory leaks
+	// onDestroy(unsubscribe);
 </script>
 
-<h1>The count is {count_value}</h1>
+<!-- $count is for shortcut subscribe/listen store data, and without memory leaks -->
+<h1>The count is {$count}</h1>
 
 <Incrementer />
 <Decrementer />
