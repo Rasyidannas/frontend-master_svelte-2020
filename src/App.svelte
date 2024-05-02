ed2c67ffe6cb88e250b322cde8a0ca4bc85e4d64
Rasyidannas <rasshit.dsgn@gmail.com>
2024-05-02 07:25:14 +0700

    4/4. Transitions - Custom CSS Transitions.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,54 @@
 <script>
-	import { fade, fly } from "svelte/transition";
+	import { fade } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+
 	let visible = true;
+
+	//this is custom css transition
+	function spin(node, { duration }) {
+		return {
+			duration,
+			css: (t) => {
+				const eased = elasticOut(t);
+
+				return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${Math.trunc(t * 360)},
+						${Math.min(100, 1000 * (1 - t))}%,
+						${Math.min(50, 500 * (1 - t))}%
+					);`
+			}
+		};
+	}
 </script>
 
-<label>
+<label style="position: relative; top:-8rem">
 	<input type="checkbox" bind:checked={visible} />
 	visible
 </label>
 
 {#if visible}
-	<p in:fly={{ y:200, duration: 2000 }} out:fade>
-		Flies in, fades out
-	</p>
+	<div
+		class="centered"
+		in:spin={{ duration: 8000 }}
+		out:fade
+	>
+		<span>transitions!</span>
+	</div>
 {/if}
+
+<style>
+	.centered {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	span {
+		position: absolute;
+		transform: translate(-50%, -50%);
+		font-size: 4em;
+	}
+</style>
